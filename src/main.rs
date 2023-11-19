162cfee783258d0ca1a13d4e50087f3c3ba6a492
Ryan Breaker <ryan@breaker.rocks>
2023-11-18 19:43:53 -0600

    new endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
-use axum::routing::get;
-use axum::{http, Router};
+mod handlers;
+
+use axum::routing::{get, post};
+use axum::Router;
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let port = std::env::var("PORT").unwrap_or("3000".to_string());
     let addr = format!("0.0.0.0:{port}");
 
-    let app = Router::new().route("/", get(health));
+    let db_url = std::env::var("DATABASE_URL").expect("missing DATABASE_URL env");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db_url)
+        .await?;
+
+    let app = Router::new()
+        .route("/", get(handlers::health))
+        .route("/quotes", post(handlers::create_quote))
+        .with_state(pool);
 
     axum::Server::bind(&addr.parse().unwrap())
         .serve(app.into_make_service())
@@ -14,8 +26,4 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .unwrap();
 
     Ok(())
-}
-
-async fn health() -> http::StatusCode {
-    http::StatusCode::OK
 }
